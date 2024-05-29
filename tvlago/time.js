57f0ac0beb4b4192434bfea04526897126913471
ダニー <98234578+mrdannyarmstrong@users.noreply.github.com>
2024-05-29 12:48:29 -0400

    Add files via upload

---
+++ tvlago/time.js
@@ -0,0 +1,13 @@
+﻿function gettime() {
+    ustime = document.getElementById("ustime");
+    t = new Date();
+    min = '';
+    if (t.getMinutes() < 10) {
+       min = '0' + t.getMinutes()
+    }
+    else {
+        min = t.getMinutes()
+    }
+    fulltime = t.getHours() + ':' + min;
+    clock.innerText = fulltime
+}
