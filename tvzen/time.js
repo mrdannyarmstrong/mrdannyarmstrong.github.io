98919351ec8d4e40be3ace119edf527adc01c163
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2025-03-05 16:44:06 -0500

    Add files via upload

---
+++ tvzen/time.js
@@ -0,0 +1,26 @@
+﻿function gettime() {
+    t = new Date();
+    min = '';
+    if (t.getMinutes() < 10) {
+       min = '0' + t.getMinutes()
+    }
+    else {
+        min = t.getMinutes()
+    }
+    fulltime = t.getHours() + ':' + min;
+    document.getElementById("time").innerText = fulltime
+}
+
+function gettime2() {
+    time = document.getElementById("time");
+    t = new Date();
+    min = '';
+    if (t.getMinutes() < 10) {
+       min = '0' + t.getMinutes()
+    }
+    else {
+        min = t.getMinutes()
+    }
+    fulltime = t.getHours() + ':' + min;
+    time.innerText = fulltime
+}
