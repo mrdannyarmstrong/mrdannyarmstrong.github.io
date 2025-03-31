4a8de5ed4af3913245f8609cea30df27a313cffd
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2025-03-30 21:04:32 -0400

    Add files via upload

--- tvneu/time.js
+++ tvneu/time.js
@@ -11,3 +11,81 @@
     fulltime = t.getHours() + ':' + min;
     time.innerText = fulltime
 }
+
+function holiday() {
+	d = new Date();
+	day=d.getDay();
+	month=d.getMonth() + 1;
+	if (month == 5 & day == 10) {
+		switchbutt.style.display="none";
+		bg.style.backgroundImage="url(https://backend-mrdannyarmstrong.github.io/tvneu/v2/holiday/bday.jpg)";
+		time.style.color="black"
+	}
+	if (month == 1 & day == 1) {
+		switchbutt.style.display="none";
+		bg.style.backgroundImage="url(https://backend-mrdannyarmstrong.github.io/tvneu/v2/holiday/newyear.jpg)";
+		time.style.color="black"
+	}
+	
+	if (month == 2 & day == 14) {
+		switchbutt.style.display="none";
+		bg.style.backgroundImage="url(https://backend-mrdannyarmstrong.github.io/tvneu/v2/holiday/valentines.jpg)";
+		time.style.color="black"
+	}
+	
+	if (month == 3 & day == 17) {
+		switchbutt.style.display="none";
+		bg.style.backgroundImage="url(https://backend-mrdannyarmstrong.github.io/tvneu/v2/holiday/stpatricks.jpg)";
+		time.style.color="black"
+	}
+	
+	if (month == 4 & day >= 1 & day <= 7) {
+		switchbutt.style.display="none";
+		bg.style.backgroundImage="url(https://backend-mrdannyarmstrong.github.io/tvneu/v2/holiday/spring.jpg)";
+		time.style.color="black"
+	}
+	
+	if (month == 7 & day == 4) {
+		switchbutt.style.display="none";
+		bg.style.backgroundImage="url(https://backend-mrdannyarmstrong.github.io/tvneu/v2/holiday/usaindp.jpg)";
+		time.style.color="black"
+	}
+	
+	if (month == 9 & day >= 1 & day <= 7) {
+		switchbutt.style.display="none";
+		bg.style.backgroundImage="url(https://backend-mrdannyarmstrong.github.io/tvneu/v2/holiday/spring.jpg)";
+		time.style.color="black"
+	}
+	
+	if (month == 10 & day == 31) {
+		switchbutt.style.display="none";
+		bg.style.backgroundImage="url(https://backend-mrdannyarmstrong.github.io/tvneu/v2/holiday/halloween.jpg)";
+		time.style.color="black"
+	}
+	
+	if (month == 11 & day >= 25 & day <= 29) {
+		switchbutt.style.display="none";
+		bg.style.backgroundImage="url(https://backend-mrdannyarmstrong.github.io/tvneu/v2/holiday/thanksgiving.jpg)";
+		time.style.color="black"
+	}
+	
+	
+	
+	if (month == 12 & day == 25) {
+		switchbutt.style.display="none";
+		bg.style.backgroundImage="url(https://backend-mrdannyarmstrong.github.io/tvneu/v2/holiday/christmas.jpg)";
+		time.style.color="black"
+	}
+	
+	if (month == 12 & day == 30) {
+		switchbutt.style.display="none";
+		bg.style.backgroundImage="url(https://backend-mrdannyarmstrong.github.io/tvneu/v2/holiday/newyear.jpg)";
+		time.style.color="black"
+	}
+	
+	if (month == 12 & day == 31) {
+		switchbutt.style.display="none";
+		bg.style.backgroundImage="url(https://backend-mrdannyarmstrong.github.io/tvneu/v2/holiday/newyear.jpg)";
+		time.style.color="black"
+	}
+}
