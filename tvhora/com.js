07c8569077e77bfb2bc1c4618311069143b75fee
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2026-01-01 16:15:05 -0400

    Add files via upload

--- tvhora/com.js
+++ tvhora/com.js
@@ -1,38 +1,35 @@
-function docom() {
-    needupdate = checkupdate();
-    if (needupdate == 1) {
-        url = geturl();
-        const topbar = document.getElementById("clock").style.display = "none";
-        const mainui = document.getElementById("mainui").innerHTML = '<h1>An update is required</h1><h2>Press update to download the newest version</h2><a href="' + url + '"><button>Update</button></a>'
+function maxsize(bandeira) {
+    if (bandeira == 1) {
+        wnum = window.innerWidth;
+        hnum = window.innerHeight;
+        video.style.width = wnum;
+        video.style.height = hnum;
     }
-    if (needupdate == 0) {
-        const mainui = document.getElementById("mainui").innerHTML = '';
-        channels = [];
-        channels = getchannels();
-        for (var i = 0; i < channels.length; i++) {  
-            if (channels[i][0] == "NULL") {
-                const mainui = document.getElementById("mainui").innerHTML += '<div class="channel" onmouseover="dohoversfx()"></div>';
-            }
-            if (channels[i][0] == "index.html") {
-                const mainui = document.getElementById("mainui").innerHTML += '<div style="width: 0; height 0; display: none;"></div>';
-            }
-            else {
-                const mainui = document.getElementById("mainui").innerHTML += '<div class="channel" onmouseover="dohoversfx()"><a href="'  + channels[i][0] +  '"><img src="'  + channels[i][1] +  '" class="logo"></a></div>';
-            }
-        }
+    if (bandeira == 2) {
+        wnum = window.innerWidth;
+        hnum = window.innerHeight;
+        video.style.width = wnum;
+        video.style.height = hnum;
+        video.style.marginTop = "-21px";
     }
 }
 
 function dohoversfx() {
-    hoversfx.play();
+    hoversfx.play()
 }
 
-function maxsize() {
-    wnum = window.innerWidth;
-    hnum = window.innerHeight;
-    video.style.width = wnum;
-    video.style.height = hnum;
-    video.style.marginTop = "-21px";
+function getdb(chvalue) {
+    const chdb = getchinfo();
+    chnum = ""
+    if (chvalue < 10) {
+        chnum = "0" + chvalue
+    }
+    if (chvalue >= 10) {
+        chnum = chvalue
+    }
+    newstr = chnum + " " + chdb[chvalue][0]
+    channelname.innerText = newstr
+    channeldesc.innerText = chdb[chvalue][1] 
 }
 
 function getcurpath() {
@@ -40,4 +37,21 @@ function getcurpath() {
     urlbox = fullurl.split("#");
     let cururl = urlbox[1];
     return cururl;
+}
+
+function docom() {
+    const mainui = document.getElementById("mainui").innerHTML = '';
+    channels = [];
+    channels = getchannels();
+    for (var i = 0; i < channels.length; i++) {  
+        if (channels[i][0] == "NULL") {
+            const mainui = document.getElementById("mainui").innerHTML += '<div class="channel" onmouseover="dohoversfx()"></div>';
+        }
+        if (channels[i][0] == "index.html") {
+            const mainui = document.getElementById("mainui").innerHTML += '<div style="width: 0; height 0;"></div>';
+        }
+    else {
+            const mainui = document.getElementById("mainui").innerHTML += '<div class="channel"  onmouseover="dohoversfx()"><a href="'  + channels[i][0] +  '"><img src="'  + channels[i][1] +  '" class="logo"></a></div>';
+        }
+    }
 }
