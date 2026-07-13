572794842e40f02420fc28320e3cfd11530422af
Daniel <98234578+mrdannyarmstrong@users.noreply.github.com>
2026-07-12 22:08:36 -0400

    Add files via upload

---
+++ sumime/roman/dict.js
@@ -0,0 +1,10 @@
+function getimedict() {
+    imedict = [
+        ["𒁹", "diš"],
+        ["𒈫", "min"],
+        ["𒐈", "eš"],
+        ["𒐉", "limmu"],
+        ["𒐊", "ia"],
+    ];
+    return imedict
+}
